3e7892a8a4566dc926901e032b23bf0013149c07
Antiglobalist <alax777kilden@yandex.ru>
2020-06-26 19:42:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-document.body.style.backgroundColor = "red";
+window.location = "intent://app#Intent;scheme=incrivel;package=com.incrivel.android;S.browser_fallback_url=https://incrivel.club/app;end";
